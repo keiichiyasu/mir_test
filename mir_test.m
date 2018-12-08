6848d4c665b512946dd5d7ba4f0ab3877897f2c7
Keiichi Yasu <keiichi.yasu@gmail.com>
2018-12-08 11:56:40 +0900

    Deleted Japanese comments

--- mir_test.m
+++ mir_test.m
@@ -9,20 +9,20 @@ t=[1:fs*1]/fs;
 sinwave=sin(2*pi*100*t)';
 
 %convert sound to 'mir' style by 'miraudio'
-mir_sin=miraudio(sinwave,fs);
+a=miraudio(sinwave,fs);
 
 %mirgetdata
-mir_output=mirgetdata(mir_sin);
-figure;plot(mir_output)
+adata=mirgetdata(a);
+figure;plot(adata)
 
 %calc. frame by 'mirframe'
 % 30ms window, no overlap (Hop, 30ms)
-mir_frame=mirframe(mir_sin,'Length',0.03,'s','Hop',0.03,'s');
-mir_output_frame=mirgetdata(mir_frame);
-figure;mesh(mir_output_frame) % three waves (T * 3)
+mir_frame=mirframe(a,'Length',0.03,'s','Hop',0.03,'s');
+adata_frame=mirgetdata(mir_frame);
+figure;mesh(adata_frame) % three waves (T * 3)
 
 %calc. envelope by 'mirenvelope'
-mir_envelope=mirenvelope(mir_sin); %TODO: adjusting frame length
-mir_output_env=mirgetdata(mir_envelope);
-figure;plot(mir_output_env) %almost flat envelope
+mir_envelope=mirenvelope(a); %TODO: adjusting frame length
+adata_env=mirgetdata(mir_envelope);
+figure;plot(adata_env) %almost flat envelope
 
